ef73bf3e53d469b9a74936d08927563f2ebbf1d8
Fausto Leyva <fausto.lc.29@gmail.com>
2023-02-27 18:17:56 -0600

    First commit

---
+++ src/test/java/com/platzi/tests/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.tests.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
